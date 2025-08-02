4a65cb378cad9c49cf2d9b3bac8bf752fe6e0395
ToanfKhoa <quachbuitoankhoa@gmail.com>
2025-08-02 09:21:09 +0700

    Zoetrope mechanic test

---
+++ Assets/circle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circle : MonoBehaviour
+{
+    // Vận tốc góc (độ mỗi giây)
+    public float rotationSpeed = 90f;
+
+
+    void Update()
+    {
+        // Quay quanh trục Z
+        transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+    }
+}
